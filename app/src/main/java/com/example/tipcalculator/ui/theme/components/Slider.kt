16ca4e4280382062bfda435eefeadd69e9324c5c
sblockwel <63020705+sblockwel@users.noreply.github.com>
2022-03-26 22:17:50 -0300

    ajustes

--- app/src/main/java/com/example/tipcalculator/ui/theme/components/Slider.kt
+++ app/src/main/java/com/example/tipcalculator/ui/theme/components/Slider.kt
@@ -2,10 +2,14 @@ package com.example.tipcalculator.ui.theme.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.core.text.isDigitsOnly
 
 @Composable
 fun informaPercent(valor: Int, onPercentChange: (Int) -> Unit) {
@@ -17,9 +21,23 @@ fun informaPercent(valor: Int, onPercentChange: (Int) -> Unit) {
         Slider(
             value = valor.toFloat(),
             onValueChange = { onPercentChange(it.toInt()) },
-            valueRange = 0f..100f,
-            steps = 10
+            valueRange = 0f..30f,
+
         )
+        /*OutlinedTextField(
+            value = valor.toString(),
+            onValueChange = {
+                var valorChange = valor
+                if (it.length == 0) {
+                    valorChange = 0
+                } else {
+                    if (it.isDigitsOnly() && it.length > 0) {
+                        valorChange = it.toInt()
+                    }
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )*/
     }
 
 }
